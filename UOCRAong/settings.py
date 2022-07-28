b16fcd08cdb1033c5b46f3919c1b433e50f18c65
[ tatiana ] <tatianamcloughlin@gmail.com>
2022-07-27 22:50:51 -0300

    2

--- UOCRAong/settings.py
+++ UOCRAong/settings.py
@@ -118,6 +118,8 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
+DISABLE_COLLECTSTATIC=1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
