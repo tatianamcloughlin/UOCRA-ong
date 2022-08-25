5c5cba0312ae1a8f8586d16937be27980cbe1883
[ tatiana ] <tatianamcloughlin@gmail.com>
2022-08-25 19:34:34 -0300

    heroku8

--- UOCRAong/produccion.py
+++ UOCRAong/produccion.py
@@ -8,12 +8,16 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'd3ndsqav6n99th',
+        'USER': 'pxkvcfexeyrmsv',
+        'PASSWORD': 'a7c65ecc5c8f5039b8e02714cc9dafadf1563b57ff2bd8d2fc2f3630bdb1f3cf',
+        'HOST': 'ec2-34-227-135-211.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 import django_heroku
 django_heroku.settings(locals())
