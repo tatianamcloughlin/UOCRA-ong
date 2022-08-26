9560437426931e9cd7ff00a51018f18459aa0f0c
Benjamin <benjidfer@gmail.com>
2022-08-26 16:50:40 -0300

    commit2

--- UOCRAong/produccion.py
+++ UOCRAong/produccion.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
