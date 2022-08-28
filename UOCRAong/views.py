b74e881149b7c02bb28d09e58dfeea9d65eaf4fc
Benjamin <benjidfer@gmail.com>
2022-08-28 17:34:08 -0300

    commit heroku9

--- UOCRAong/views.py
+++ UOCRAong/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from apps.noticias.models import Noticias,Categorias
 from apps.cursos.models import Cursos
+from apps.cursos.models import Usuario
 from UOCRAong.sesion import login
 from datetime import datetime
 from django.contrib.auth.hashers import make_password
@@ -49,10 +50,17 @@ def bienvenida (request):
         try:
 
             #(`password`, `last_login`, `email`, `fecha`, `usuario_activo`, `es_admin`, `apellido`, `direccion`, `dni`, `nombre`, `ciudad`, `cuil`, `provincia`, `usuario`)
-            with connection.cursor() as cursor:
-                cursor.execute(f"INSERT INTO `usuario_usuario` (`password`, `last_login`, `email`, `fecha`, `usuario_activo`, `es_admin`, `apellido`, `direccion`, `dni`, `nombre`, `ciudad`, `cuil`, `provincia`, `usuario`,`restablecer`) VALUES ('{make_password(request.POST.get('password1'))}', NULL , '{request.POST.get('email')}', '{datetime.now()}', '0', '0', '{request.POST.get('apellido')}', '{request.POST.get('direccion')}', '{request.POST.get('dni')}', '{request.POST.get('nombre_formulario')}', '{request.POST.get('ciudad')}', '{request.POST.get('dni')}', '{request.POST.get('provincia')}', '{request.POST.get('usuario')}','0')")
+            #with connection.cursor() as cursor:
+                #cursor.execute(f"INSERT INTO `usuario_usuario` (`password`, `last_login`, `email`, `fecha`, `usuario_activo`, `es_admin`, `apellido`, `direccion`, `dni`, `nombre`, `ciudad`, `cuil`, `provincia`, `usuario`,`restablecer`) VALUES ('{make_password(request.POST.get('password1'))}', NULL , '{request.POST.get('email')}', '{datetime.now()}', '0', '0', '{request.POST.get('apellido')}', '{request.POST.get('direccion')}', '{request.POST.get('dni')}', '{request.POST.get('nombre_formulario')}', '{request.POST.get('ciudad')}', '{request.POST.get('dni')}', '{request.POST.get('provincia')}', '{request.POST.get('usuario')}','0')")
+            insertar= Usuario()
+            insertar.nombre = 'benjamin'
+            insertar.apellido = 'gomez'
+            insertar.password = make_password('root')
+            insertar.email ='benjidfer@gmail.com'
+
+            insertar.save()
                 
-                row = cursor.fetchone()
+                #row = cursor.fetchone()
         except BaseException as e:
             mensaje_formulario = e.args[0]
             
