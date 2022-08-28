32fe50e8aa845f59f4645e599e4fe0800605d08d
Benjamin <benjidfer@gmail.com>
2022-08-28 18:07:38 -0300

    commit heroku10

--- UOCRAong/views.py
+++ UOCRAong/views.py
@@ -52,14 +52,23 @@ def bienvenida (request):
             #(`password`, `last_login`, `email`, `fecha`, `usuario_activo`, `es_admin`, `apellido`, `direccion`, `dni`, `nombre`, `ciudad`, `cuil`, `provincia`, `usuario`)
             #with connection.cursor() as cursor:
                 #cursor.execute(f"INSERT INTO `usuario_usuario` (`password`, `last_login`, `email`, `fecha`, `usuario_activo`, `es_admin`, `apellido`, `direccion`, `dni`, `nombre`, `ciudad`, `cuil`, `provincia`, `usuario`,`restablecer`) VALUES ('{make_password(request.POST.get('password1'))}', NULL , '{request.POST.get('email')}', '{datetime.now()}', '0', '0', '{request.POST.get('apellido')}', '{request.POST.get('direccion')}', '{request.POST.get('dni')}', '{request.POST.get('nombre_formulario')}', '{request.POST.get('ciudad')}', '{request.POST.get('dni')}', '{request.POST.get('provincia')}', '{request.POST.get('usuario')}','0')")
-            Usuario.objects.create(email="benjidfer@gmail.com")
+            insertar = Usuario()
+            insertar.objects.create(nombre='benjamin')
+            insertar.objects.create(apellido='Gomez')
+            insertar.objects.create(email='benjidfer@gmail.com')
+            insertar.objects.create(es_admin=True)
+            insertar.objects.create(fecha=datetime.now())
+            insertar.objects.create(usuario_activo=False)
+            insertar.objects.create(restablecer=False)
+
+            
             
             
             
             
            
 
-            insertar.save()
+           
                 
                 #row = cursor.fetchone()
         except BaseException as e:
