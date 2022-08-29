b90d158bfaf5cc709e7101f03f19f1f842defeaf
[ tatiana ] <tatianamcloughlin@gmail.com>
2022-08-28 22:17:39 -0300

    sabado presentacion

--- UOCRAong/wsgi.py
+++ UOCRAong/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UOCRAong.produccion')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UOCRAong.local')
 
 application = get_wsgi_application()
